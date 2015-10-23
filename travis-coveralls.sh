3f12fd78e9ae81da4e624c014b75ffcd53f4f444
Si Beaumont <simon.beaumont@citrix.com>
2015-10-23 21:19:21 +0100

    coveralls: Handle tests run in sub-directories
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- travis-coveralls.sh
+++ travis-coveralls.sh
@@ -31,6 +31,8 @@ eval ${BUILD}
 find . -name bisect* | xargs rm -f
 eval ${TEST}
 
+find . -name bisect* -exec mv {} . \;
+
 bisect-ppx-report bisect*.out -I _build -text report
 bisect-ppx-report bisect*.out -I _build -summary-only -text summary
 (cd _build; bisect-ppx-report ../bisect*.out -html ../report-html)
